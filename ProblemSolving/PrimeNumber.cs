f2d5fbee8f0b3d26274fc050eff3a2c828628236
Mosharrof Hossian Shishir <012shishir@gmail.com>
2026-03-15 02:37:13 +0600

    ...

---
+++ ProblemSolving/PrimeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    public class PrimeNumber
+    {
+        public static bool CheckPrimeNumber(int number)
+        {
+           if(number <= 1 ) return false;
+
+           if(number == 2 || number == 3) return true;
+
+           if(number % 2 == 0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2) 
+            {
+                if(number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
